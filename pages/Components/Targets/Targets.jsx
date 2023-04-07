707dc19432ab38c37afa36efd6fcbaccca2e1140
behroz <behrozsedighisf@gmail.com>
2023-04-07 20:58:31 +0330

    update

--- pages/Components/Targets/Targets.jsx
+++ pages/Components/Targets/Targets.jsx
@@ -1,13 +1,13 @@
 import { Grid } from "@mui/material";
 import BoxData from "./box/BoxData";
 const Targets = ()=>{
-    const dataBox1 = ["Express backend","Database : Mysql, Redis, MongoDb","Fluent in Amazon's Object Storage S3","Designing and implementing complex databases and experience working with SQL and NoSQL"];
+    const dataBox1 = ["Express backend","Database : Mysql, Redis, MongoDb","Fluent in Amazon's Object Storage S3","Database design and implementation"];
     const dataBox2 = ["Industrial Automation Engineering field","Learning HTML and CSS","Learning PHP and JavaScript","Building a website with React"];
     return(
             <Grid container direction="row" justifyContent="center" alignItems="center" sx={{mt:"10px",mb:"50px"}}>
                 <Grid item container xs={12} md={8} justifyContent="center" alignItems="center" spacing={2} >
                     <Grid item xs={12} md={6} >
-                        <BoxData data={dataBox1} title="My progress stages" />
+                        <BoxData data={dataBox1} title="Web specialties" />
                     </Grid>
                     <Grid item xs={12} md={6}>
                         <BoxData data={dataBox2} title="My progress stages" />
--- pages/Components/Targets/box/BoxData.jsx
+++ pages/Components/Targets/box/BoxData.jsx
@@ -6,37 +6,43 @@ const BoxData = (props)=>{
     }
     return(
         <>
-            <Grid item  container direction="row" justifyContent="center" sx={{borderRadius:"12px",backgroundColor:"customColor.BgElement",padding:"20px"}} >
-                <Grid  item sx={{width:"10px",mr:"5px"}}>
-                        <Grid item sx={{width:"15px",height:"15px",backgroundColor:"#f6d251",borderRadius:"12px"}} ></Grid>
-                        <Grid item sx={{height:"50px",width:"2px",backgroundColor:"#f6d251",marginLeft:"6px"}}></Grid>
-                        <Grid item sx={{width:"15px",height:"15px",backgroundColor:"#f6d251",borderRadius:"12px"}} ></Grid>
-                        <Grid item sx={{height:"50px",width:"2px",backgroundColor:"#f6d251",marginLeft:"6px"}}></Grid>
-                        <Grid item sx={{width:"15px",height:"15px",backgroundColor:"#f6d251",borderRadius:"12px"}} ></Grid>
-                        <Grid item sx={{height:"50px",width:"2px",backgroundColor:"#f6d251",marginLeft:"6px"}}></Grid>
-                        <Grid item sx={{width:"15px",height:"15px",backgroundColor:"#f6d251",borderRadius:"12px"}} ></Grid>
+            <Grid item  container direction="row" justifyContent="center" sx={{borderRadius:"12px",backgroundColor:"customColor.BgElement",padding:"20px",height:"100%"}} >
+                <Grid item xs={12} sx={{mb:"40px"}} >
+                    <Typography component="p" sx={{fontSize:"13px",mt:"20px",fontSize:"17px"}}>{props.title}</Typography>
                 </Grid>
-                <Grid  item xs={9} sx={{width:"10px",ml:"10px"}}>
-                        {
-                            props?.data?.map((x,index)=>{
-                                return(
-                                    <Grid key={x}>
-                                        <Typography variant="p" sx={{fontSize:"13px",mt:"20px",textAlign:"center"}}>{x}</Typography>
-                                        {
-                                            props?.data.length -1 > index && 
-                                            <Grid item sx={dada}></Grid>
-                                        }
-                                        
-                                    </Grid>
-                                )
-                            })
-                        }
-                        
-                        {/* <Typography variant="p" sx={{fontSize:"13px",mt:"20px",textAlign:"center"}}>Industrial Automation Engineering field</Typography>
-                        <Grid item sx={dada}></Grid>
-                        <Typography variant="p" sx={{fontSize:"13px",mt:"20px",textAlign:"center"}}>Industrial Automation Engineering field</Typography>
-                        <Grid item sx={dada}></Grid>
-                        <Typography variant="p" sx={{fontSize:"13px",mt:"20px",textAlign:"center"}}>Industrial Automation Engineering field</Typography> */}
+                <Grid item xs={12} container direction="row" >
+
+                    <Grid  item sx={{width:"10px",mr:"5px"}}>
+                            <Grid item sx={{width:"15px",height:"15px",backgroundColor:"#f6d251",borderRadius:"12px"}} ></Grid>
+                            <Grid item sx={{height:"50px",width:"2px",backgroundColor:"#f6d251",marginLeft:"6px"}}></Grid>
+                            <Grid item sx={{width:"15px",height:"15px",backgroundColor:"#f6d251",borderRadius:"12px"}} ></Grid>
+                            <Grid item sx={{height:"50px",width:"2px",backgroundColor:"#f6d251",marginLeft:"6px"}}></Grid>
+                            <Grid item sx={{width:"15px",height:"15px",backgroundColor:"#f6d251",borderRadius:"12px"}} ></Grid>
+                            <Grid item sx={{height:"50px",width:"2px",backgroundColor:"#f6d251",marginLeft:"6px"}}></Grid>
+                            <Grid item sx={{width:"15px",height:"15px",backgroundColor:"#f6d251",borderRadius:"12px"}} ></Grid>
+                    </Grid>
+                    <Grid  item xs={9} sx={{width:"10px",ml:"10px"}}>
+                            {
+                                props?.data?.map((x,index)=>{
+                                    return(
+                                        <Grid key={x}>
+                                            <Typography variant="p" sx={{fontSize:"13px",mt:"20px",textAlign:"center",fontWeight:"200"}}>{x}</Typography>
+                                            {
+                                                props?.data.length -1 > index && 
+                                                <Grid item sx={dada}></Grid>
+                                            }
+                                            
+                                        </Grid>
+                                    )
+                                })
+                            }
+                            
+                            {/* <Typography variant="p" sx={{fontSize:"13px",mt:"20px",textAlign:"center"}}>Industrial Automation Engineering field</Typography>
+                            <Grid item sx={dada}></Grid>
+                            <Typography variant="p" sx={{fontSize:"13px",mt:"20px",textAlign:"center"}}>Industrial Automation Engineering field</Typography>
+                            <Grid item sx={dada}></Grid>
+                            <Typography variant="p" sx={{fontSize:"13px",mt:"20px",textAlign:"center"}}>Industrial Automation Engineering field</Typography> */}
+                    </Grid>
                 </Grid>
             </Grid>
         </>
