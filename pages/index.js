dbbd4cb455046593bb6aaf0b8042a7d1c9080b70
behroz sedighi <behrozsedighisf@gmail.com>
2024-04-24 01:32:14 +0330

    fix bug

--- pages/index.js
+++ pages/index.js
@@ -61,7 +61,7 @@ export async function getStaticProps(context) {
     return data
   }
   
-  const data = await getContributions(process.env.token, 'behrozsedighi')
+  const data = await getContributions(process.env.TOKEN, 'behrozsedighi')
 
 
 
