4f5c83a736b4b82312bfa72a75554d64c5cc87b9
behroz sedighi <behrozsedighisf@gmail.com>
2024-04-24 02:18:03 +0330

    update

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,8 @@ export default function Home(props) {
       </Head>
       <Header />
       <AboutMe />
-      <GithubInfo data={props?.data} />
+      {props?.data && <GithubInfo data={props?.data} />}
+      
       <Targets />
     </>
   )
@@ -29,43 +30,43 @@ export default function Home(props) {
 
 
 
-export async function getStaticProps(context) {
-  async function getContributions(token, username) {
-    const headers = {
-        'Authorization': `bearer ${token}`,
-    }
-    const body = {
-        "query": `query {
-            user(login: "${username}") {
-              name
-              contributionsCollection {
-                contributionCalendar {
-                  colors
-                  totalContributions
-                  weeks {
-                    contributionDays {
-                      color
-                      contributionCount
-                      date
-                      weekday
-                    }
-                    firstDay
-                  }
-                }
-              }
-            }
-          }`
-    }
-    const response = await fetch('https://api.github.com/graphql', { method: 'POST', body: JSON.stringify(body), headers: headers })
-    const data = await response.json()
-    return data
-  }
+// export async function getStaticProps(context) {
+//   async function getContributions(token, username) {
+//     const headers = {
+//         'Authorization': `bearer ${token}`,
+//     }
+//     const body = {
+//         "query": `query {
+//             user(login: "${username}") {
+//               name
+//               contributionsCollection {
+//                 contributionCalendar {
+//                   colors
+//                   totalContributions
+//                   weeks {
+//                     contributionDays {
+//                       color
+//                       contributionCount
+//                       date
+//                       weekday
+//                     }
+//                     firstDay
+//                   }
+//                 }
+//               }
+//             }
+//           }`
+//     }
+//     const response = await fetch('https://api.github.com/graphql', { method: 'POST', body: JSON.stringify(body), headers: headers })
+//     const data = await response.json()
+//     return data
+//   }
   
-  const data = await getContributions( process.env.TOKEN, 'behrozsedighi')
+//   const data = await getContributions( TOKEN, username)
 
 
 
-  return {
-    props: {data}, // will be passed to the page component as props
-  }
-}
+//   return {
+//     props: {data}, // will be passed to the page component as props
+//   }
+// }
